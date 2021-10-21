a00d1653319bad5a7402071c04b2f6d0ad0ee060
Elena Konovalenko <olena.konovalenko@privatbank.ua>
2021-10-21 16:59:11 +0300

    Программа Бинго выполнена

--- src/com/pb/ekonovalenko/hm3/Bingo.java
+++ src/com/pb/ekonovalenko/hm3/Bingo.java
@@ -9,27 +9,33 @@ public class Bingo {
         int x;
         x = r.nextInt(101);
         int y;
-        int counter;
-        
+        int counter=0;
+
 
         Scanner scn = new Scanner(System.in);
         System.out.println("Введите целое число из диапазона от 0 до 100");
+        System.out.println("Если Вам надоело играть введите -1");
 
         do {
             y = scn.nextInt();
-            if (y <= 0 || y >= 100) {
+            counter++;
+            if (-1 == y) {
+                System.out.println("Жалко, что вы сдались! Игра окончена");
+                break;}
+            else if (y <= 0 || y >= 100) {
                 System.out.println("Вы ввели число не из заданного диапазона ");
                 System.out.println("Повторите ввод");
-            } else if (y > x) {
+            }
+            else if (y > x) {
                 System.out.println("Задуманое число меньше введенного");
                 System.out.println("Повторите ввод числа");
             } else if (y < x) {
                 System.out.println("Задуманое число больше введенного");
                 System.out.println("Повторите ввод числа");
-                counter = 1;
             } else if (y == x) {
                 System.out.println("ПОЗДРАВЛЯЮ! Вы угадали число");
                 System.out.println("Игра окончена");
+                System.out.println("Вы угодали за  " + counter);
             }
 
         }
