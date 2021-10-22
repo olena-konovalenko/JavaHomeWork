d3edac4d43c06baac06e836a247cae51ba162c12
Elena Konovalenko <olena.konovalenko@privatbank.ua>
2021-10-22 20:52:40 +0300

    До вычисления количества положительных

--- src/com/pb/ekonovalenko/hm3/Array.java
+++ src/com/pb/ekonovalenko/hm3/Array.java
@@ -1,4 +1,21 @@
 package com.pb.ekonovalenko.hm3;
 
+import java.util.Scanner;
+
 public class Array {
+    public static void main(String[] args) {
+        int nums[] = new int[10];
+        int sum = 0;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите 10 любых целых чисел");
+        for (int y = 0; y <= 9; y++) {
+            nums[y] = scan.nextInt();
+        }
+        for (int i = 0; i <= 9; i++) {
+            System.out.println(nums[i]);
+            sum = nums[i] + sum;
+        }
+        System.out.println("Сумма массива равна "+ sum);
+    }
 }
