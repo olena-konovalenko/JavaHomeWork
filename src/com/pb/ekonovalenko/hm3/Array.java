0d01b393d2f253d49a72e794e92030a1d69bc07d
Elena Konovalenko <olena.konovalenko@privatbank.ua>
2021-10-23 15:45:28 +0300

    Пузырьки

--- src/com/pb/ekonovalenko/hm3/Array.java
+++ src/com/pb/ekonovalenko/hm3/Array.java
@@ -5,23 +5,39 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         int nums[] = new int[10];
+
         int sum = 0;
         int counter = 0;
         Scanner scan = new Scanner(System.in);
 
-
         System.out.println("Введите 10 любых целых чисел");
+
         for (int y = 0; y <= 9; y++) {
             nums[y] = scan.nextInt();
         }
         for (int i = 0; i <= 9; i++) {
-            System.out.println(nums[i]);
+            System.out.print(nums[i]+ " ");
             sum = nums[i] + sum;
-            if (nums[i]<0){
+            if (nums[i] < 0) {
                 counter++;
             }
         }
-        System.out.println("Сумма массива равна "+ sum);
+
+        System.out.println("Сумма массива равна " + sum);
         System.out.println("Количество отлицательных чисел = " + counter);
+
+        for (int i=0; i<9; i++) {
+            if (nums[i]>nums[i+1]) {
+                int kol = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = kol;
+            }
+        }
+
+        System.out.println("отсортированний массив");
+        for (int i = 0; i <= 9; i++) {
+            System.out.print(nums[i]+" ");
+        }
     }
 }
+
